82e88f92a4856af1522037de66ade4ac3ad56513
michalpiaskowski15 <258956@student.pwr.edu.pl>
2021-03-18 11:46:11 +0100

    dodano przeciazenia operatorow arytmetycznych

--- src/LZespolona.cpp
+++ src/LZespolona.cpp
@@ -58,3 +58,56 @@ LZespolona  operator / (LZespolona  Skl1,  double  Skl2){
   Wynik.im = Skl1.im / Skl2;
   return Wynik;
 }
+
+/*
+ * Realizuje odejmowanie dwoch liczb zespolonych.
+ * Argumenty:
+ *    Skl1 - pierwszy skladnik odejmowania,
+ *    Skl2 - drugi skladnik odejmowania.
+ * Zwraca:
+ *    Roznice dwoch skladnikow przekazanych jako parametry.
+ */
+LZespolona  operator - (LZespolona  Skl1,  LZespolona  Skl2)
+{
+  LZespolona  Wynik;
+
+  Wynik.re = Skl1.re - Skl2.re;
+  Wynik.im = Skl1.im - Skl2.im;
+  return Wynik;
+}
+
+
+/*!
+ * Realizuje mnozenie dwoch liczb zespolonych.
+ * Argumenty:
+ *    Skl1 - pierwszy skladnik mnozenia,
+ *    Skl2 - drugi skladnik mnozenia.
+ * Zwraca:
+ *    Iloczyn dwoch skladnikow przekazanych jako parametry.
+ */
+LZespolona  operator * (LZespolona  Skl1,  LZespolona  Skl2)
+{
+  LZespolona  Wynik;
+
+  Wynik.re = ((Skl1.re*Skl1.im)-(Skl2.re*Skl2.im));
+  Wynik.im = ((Skl1.re*Skl2.im)+(Skl2.re*Skl1.im));
+  return Wynik;
+}
+
+
+/*!
+ * Realizuje dzielenie dwoch liczb zespolonych.
+ * Argumenty:
+ *    Skl1 - pierwszy skladnik dzielenia,
+ *    Skl2 - drugi skladnik dzielenia.
+ * Zwraca:
+ *    Iloraz dwoch skladnikow przekazanych jako parametry.
+ */
+LZespolona  operator / (LZespolona  Skl1,  LZespolona  Skl2)
+{
+  LZespolona  Wynik;
+
+  Wynik.re = ((Skl1.re*Skl2.re)+(Skl1.im*Skl2.im))/(pow(Skl2.re, 2)+pow(Skl2.im, 2));           /*korzystam z przeksztalconego wzoru sprzezenia*/
+  Wynik.im = ((Skl1.im*Skl2.re)-(Skl1.re*Skl2.im))/(pow(Skl2.re, 2)+pow(Skl2.im, 2));
+  return Wynik;
+}

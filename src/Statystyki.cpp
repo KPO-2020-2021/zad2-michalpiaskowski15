50af79704458f0553b035a4108ef60e3432cad0f
michalpiaskowski15 <258956@student.pwr.edu.pl>
2021-03-18 20:20:46 +0100

    rozpoczeto modul statystyki

--- src/Statystyki.cpp
+++ src/Statystyki.cpp
@@ -1,7 +1,17 @@
+#include <iostream>
 #include "Statystyki.hh"
 
 
+
 /*
- * Tu nalezy zdefiniowac funkcje, ktorych zapowiedzi znajduja sie
- * w pliku naglowkowym.
+ *  Rozpoczety modul statystyki
+ *  Wyswietla ilosc poprawnych odpowiedzi, oraz procentowy wynik testu
  */
+
+void Statystyka(int Poprawne)
+{
+    std::cout << "Poprawne odpowiedzi:" << Poprawne << std::endl;
+    std::cout << "Procent poprawnych odpowiedzi:" << (Poprawne/4)*100 << "%" << std::endl;
+    
+}
+
